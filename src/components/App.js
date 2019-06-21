0cab7c35f90752bbf73bcbca9e6b80cd70eb57f1
James Martineau <James.Martineau@libertymutual.com>
2019-06-20 20:37:46 -0400

    added loading page

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,43 @@
-import React from 'react';
-import { BrowserRouter } from "react-router-dom";
+import React, {Component} from "react";
 import AppHeader from './AppHeader';
 import Landing from '../containers/Landing';
+import Loader from './Loader';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <div className="App">
-          <AppHeader />
-          <div className="app-content-container" >
-              <Landing/>
-          </div>
-      </div>
-    </BrowserRouter>
-  );
+export default class App extends Component {
+
+    state = {
+        showLoader: true,
+        time: 5
+    };
+
+    componentDidMount(){
+        let countDown = 5;
+        let intervalId = setInterval(() => {
+            countDown --;
+            this.setState({time: countDown});
+
+            if(countDown === 0){
+                clearInterval(intervalId);
+                this.setState({showLoader: false})
+            }
+        }, 1000)
+    }
+
+    render() {
+        const { time, showLoader } = this.state;
+        return (
+            <div className="App">
+                {showLoader ? (
+                    <Loader time={time}/>
+                ) : (
+                    <>
+                        <AppHeader/>
+                        <div className="app-content-container">
+                            <Landing/>
+                        </div>
+                    </>
+                )}
+            </div>
+        );
+    }
 }
-
-export default App;
---
+++ src/components/Loader.js
@@ -0,0 +1,18 @@
+import React from "react";
+import logo from '../images/teamLogo.png';
+import { Slide, Fade } from 'react-reveal';
+
+export default function Loader(props) {
+    const { time } = props;
+
+    return (
+        <Slide left opposite when={time > 1}>
+            <div className="loader-container center-align">
+                <Fade duration={2000}>
+                    <img src={logo} className='loader-img' alt='team icon' />
+                    <h5 className='header-style'>Tower gathering in: {time}</h5>
+                </Fade>
+            </div>
+        </Slide>
+    );
+}
