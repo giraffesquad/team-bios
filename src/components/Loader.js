c8f22b268486a449b897c1beed01aea7e9e821e4
James Martineau <James.Martineau@libertymutual.com>
2019-06-20 20:51:25 -0400

    styling loading page

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from '../images/teamLogo.png';
+import logo from '../images/teamLogo2.png';
 import { Slide, Fade } from 'react-reveal';
 
 export default function Loader(props) {
@@ -8,10 +8,12 @@ export default function Loader(props) {
     return (
         <Slide left opposite when={time > 1}>
             <div className="loader-container center-align">
-                <Fade duration={2000}>
-                    <img src={logo} className='loader-img' alt='team icon' />
-                    <h5 className='header-style'>Tower gathering in: {time}</h5>
-                </Fade>
+                <div className='img-wrapper'>
+                    <Fade duration={2000}>
+                        <img src={logo} className='loader-img' alt='team icon' />
+                    </Fade>
+                </div>
+                <h5 className='header-style'>Tower gathering in: {time}</h5>
             </div>
         </Slide>
     );
